e90a3a6df6545ba62b490e60f9d2b2ea0cff58c8
Joe Mellin <mellin.joe@gmail.com>
2016-10-03 11:46:22 -0700

    update binary

--- Sonoma Test/ViewController.swift
+++ Sonoma Test/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Actions
+   
 }
 
